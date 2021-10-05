66d0573c783d3e1629831f4542175ea49c42ccb0
RajParsaniya <75837613+RajParsaniya@users.noreply.github.com>
2021-10-05 10:19:55 +0530

    Add files via upload

---
+++ Pattern.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n, i, j, k, m=1;
+    printf("Enter A Row : ");
+    scanf("%d",&n);
+
+    printf("\n");
+
+    for (i = n; i >= 1; i--)
+    {        
+        for (j = 1; j < m; j++)
+        {
+            printf(" ");
+        }
+
+        for (k = 1; k <= 2*i-1; k++)
+        {
+            if (k==1 || k==2*i-1 || i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        
+        printf("\n");
+        m++;
+    }
+
+    getch();
+}
